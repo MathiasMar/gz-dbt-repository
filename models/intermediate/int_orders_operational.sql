e7b78020533aa00294b32df53177b42dfdbd5e5f
Matheus De Martin <matheuscostadmartin@gmail.com>
2025-08-08 16:31:44 +0000

    int_orders_operational creation

---
+++ models/intermediate/int_orders_operational.sql
@@ -0,0 +1,13 @@
+SELECT
+orders_id,
+date_date,
+ROUND((margin + shipping_fee - logcost - ship_cost), 2) AS operational_margin,
+quantity,
+revenue,
+purchase_cost,
+margin
+from
+{{ref("int_orders_margin")}}
+JOIN
+{{ref("stg_gz_raw_data__raw_gz_ship")}}
+USING (orders_id)
