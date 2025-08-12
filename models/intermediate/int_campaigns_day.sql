ab6f85bec8ed82a92c6cd337e67520d5a6fc32b5
Matheus De Martin <matheuscostadmartin@gmail.com>
2025-08-12 18:31:03 +0000

    finance_campaigns_day creation

--- models/intermediate/int_campaigns_day.sql
+++ models/intermediate/int_campaigns_day.sql
@@ -4,8 +4,8 @@ COUNT(paid_source) AS nb_paid_source,
 COUNT(campaign_key) AS nb_campaign_key,
 COUNT(campaign_name) AS nb_campaign_name,
 SUM(ads_cost) AS ads_cost,
-SUM(impression) AS impression,
-SUM(click) AS click
+SUM(impression) AS ads_impression,
+SUM(click) AS ads_clicks
 FROM {{ref("int_campaigns")}}
 GROUP BY date_date
 ORDER BY date_date DESC
